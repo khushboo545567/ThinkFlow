3f3ffba69ffc71cd65fd4a71eab1d0558236b267
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-12-13 16:24:20 +0530

    handling routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,30 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./layout.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisgerPage from "./pages/RegisterPage.jsx";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisgerPage />} />
+      <Route path="/" element={<></>} />
+    </Route>
+  )
+);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>
 );
