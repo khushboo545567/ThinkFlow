bbd5a17afc6120048fadbcdfed4521bd09fb06f7
Khushboo <khushbooyadav7964@gmail.com>
2025-12-19 14:07:06 +0530

    side bar done

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,12 +7,42 @@ const Sidebar = () => {
       className="
         fixed top-16 left-0
         w-64 h-[calc(100vh-4rem)]
-        bg-green-500 dark:bg-gray-800
-        text-white p-4
+        bg-gray-800 dark:bg-gray-800
+        text-white p-4 pt-16
       "
     >
-      <h2 className="text-lg font-semibold">Sidebar</h2>
-      <Link to="/profile">Profile</Link>
+      <div className="flex flex-col gap-4">
+        <Link
+          to="/"
+          className="flex gap-3 text-lg hover:bg-green-300 p-2 rounded-sm"
+        >
+          <i class="ri-home-2-fill"></i>Home
+        </Link>
+        <Link
+          to="/profile"
+          className="flex gap-3 text-lg hover:bg-green-300 p-2 rounded-sm"
+        >
+          <i class="ri-account-circle-fill"></i>Profile
+        </Link>
+        <Link
+          to="#"
+          className="flex gap-3 text-lg hover:bg-green-300 p-2 rounded-sm"
+        >
+          <i class="ri-bar-chart-fill"></i>Status
+        </Link>
+        <Link
+          to="#"
+          className="flex gap-3 text-lg hover:bg-green-300 p-2 rounded-sm"
+        >
+          <i class="ri-book-marked-fill"></i>Library
+        </Link>
+        <Link
+          to="#"
+          className="flex gap-3 text-lg hover:bg-green-300 p-2 rounded-sm"
+        >
+          <i class="ri-group-fill"></i>Following+
+        </Link>
+      </div>
     </div>
   );
 };
