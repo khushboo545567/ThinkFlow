90a1302524eb28a84b2781a6dd6b3ec72cd5cee3
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-12-24 14:22:39 +0530

    .

--- frontend/src/pages/admin/ManageUserPage.jsx
+++ frontend/src/pages/admin/ManageUserPage.jsx
@@ -68,7 +68,7 @@ const ManageAssignRoleUser = () => {
 
         <button
           type="submit"
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+          className="bg-black text-white px-4 py-2 rounded "
         >
           Assign
         </button>
